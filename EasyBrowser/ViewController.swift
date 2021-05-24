d3ee8f89dbe75b3d9eca378313b30df5fd132101
nitrocosis <nitrocosis>
2021-05-24 17:03:31 -0500

    Initial Commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Sarah Rebecca Giovine Estrellado on 5/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
